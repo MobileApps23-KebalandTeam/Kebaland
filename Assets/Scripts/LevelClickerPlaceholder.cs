6997c3d957cc21640acf5ea971430063e96961f0
Apszer <arkgaw323@gmail.com>
2023-05-03 12:14:23 +0200

    added levels placeholder scenes

---
+++ Assets/Scripts/LevelClickerPlaceholder.cs
@@ -0,0 +1,34 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelClickerPlaceholder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LevelPassed()
+    {
+        PushInfoToParent(true);
+    }
+
+    public void LevelFailed()
+    {
+        PushInfoToParent(false);
+    }
+
+    private static void PushInfoToParent(bool nextLevel)
+    {
+        LevelChoice.updateLevel(nextLevel);
+        SceneManager.LoadScene("LevelsChoiceScene");
+    }
+}
