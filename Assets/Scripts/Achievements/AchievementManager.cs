151d9d56feae3ad4e5ca83ab194d25955fc26954
JuMillerDev <juli.melnyk.2001@gmail.com>
2023-05-07 21:23:11 +0200

    Script Changes
    Added scrollbar to achievments, removed old scripts to make a better system for managing achievements

--- Assets/Scripts/Achievements/AchievementManager.cs
+++ Assets/Scripts/Achievements/AchievementManager.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AchievementsListDisplay : MonoBehaviour
+public class AchievementManager : MonoBehaviour
 {
-    public Transform targetTransform;
-    public AchievementItemDisplay itemDisplay;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
