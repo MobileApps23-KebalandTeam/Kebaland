a1cf714862f62459cc37466a03fd155bc0efb5d9
ksurdyk1 <karsur121@wp.pl>
2023-05-24 19:26:05 +0200

    Added background movement

--- Assets/Scripts/FightScreenScripts/EnemyMovement.cs
+++ Assets/Scripts/FightScreenScripts/EnemyMovement.cs
@@ -1,15 +1,33 @@
+using System;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 60f;
     [SerializeField] private GameObject target;
+    
+    [SerializeField] private GameObject player;
+    private Vector2 _playerStartingPos;
+
+    private void Start()
+    {
+        _playerStartingPos = player.transform.position;
+    }
+
     void Update()
     {
         Vector2 startPosition = transform.position;
         Vector2 targetPosition = target.transform.position;
-
-        if (startPosition != targetPosition)
+        int tapCounter = Input.touchCount;
+        if (tapCounter == 0 && player.transform.position.y > _playerStartingPos.y)
+        {
+            speed = 460f;
+        }
+        else 
+        {
+            speed = 60f;
+        }
+        if (startPosition.y < targetPosition.y)
         {
             transform.position = Vector2.MoveTowards(
                 startPosition, targetPosition, speed * Time.deltaTime);
--- Assets/Scripts/FightScreenScripts/PlayerMovement.cs
+++ Assets/Scripts/FightScreenScripts/PlayerMovement.cs
@@ -1,26 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private GameObject target;
+    [SerializeField] private GameObject background;
     [SerializeField] private float speed = 90f;
+    [SerializeField] private float backgroundSpeed = 400f;
+    private Vector2 _startingPoint;
+
+    private void Start()
+    {
+        _startingPoint = transform.position;
+    }
 
     void Update()
     {
         int tapCounter = Input.touchCount;
         Vector2 startPosition = transform.position;
         Vector2 targetPosition = target.transform.position;
-        if (tapCounter > 0 && startPosition != targetPosition)
+
+        if (tapCounter > 0 && startPosition.y < targetPosition.y)
         {
+            background.transform.position += backgroundSpeed * Time.deltaTime * Vector3.down;
             transform.position = Vector2.MoveTowards(
                 startPosition, targetPosition, speed * Time.deltaTime);
-        }
-
-        if (transform.position == target.transform.position)
-        {
+        } else if (tapCounter == 0) {
+            if (startPosition.y > _startingPoint.y)
+            {
+                transform.position = Vector2.MoveTowards(
+                    startPosition, _startingPoint, speed * Time.deltaTime);
+                background.transform.position -= (backgroundSpeed) * Time.deltaTime * Vector3.down;
+            }
+        } else {
             SceneManager.LoadScene("Scenes/LevelPlaceholder");
         }
     }
